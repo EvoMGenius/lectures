f2aa978d9fd26718be013c9fac1ebe05dd2b4993
apatios <apatrios@mail.ru>
2023-03-04 16:51:04 +1000

    Код сетевого чата

---
+++ src/main/java/f/ClientHandler.java
@@ -0,0 +1,82 @@
+package f;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    private MyServer myServer;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public ClientHandler(MyServer myServer, Socket socket) {
+        try {
+            this.myServer = myServer;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.name = socket.getInetAddress().toString();
+
+            new Thread(() -> {
+                try {
+                    myServer.subscribe(this);
+                    readMessages();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    closeConnection();
+                }
+            }).start();
+        } catch (IOException e) {
+            throw new RuntimeException("Проблемы при создании обработчика для клиента");
+        }
+    }
+
+    private void readMessages() throws IOException {
+        while (true) {
+            String s = in.readLine();
+            System.out.println("From " + name + ": " + s);
+            if (s.equals("/end")) {
+                return;
+            }
+            myServer.broadcastMsg(name + ": " + s + "\n");
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        myServer.unsubscribe(this);
+        myServer.broadcastMsg(name + "вышел из чата");
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/s/Main.java
@@ -0,0 +1,7 @@
+package s;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
